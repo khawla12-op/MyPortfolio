cf820d86fde761fd67a227ebfde31e54d257d3f9
khaoula <khawlaallak@gmail.com>
2024-09-29 11:40:30 +0100

    changing build to dist

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Set the output directory to 'build'
+    outDir: 'dist', // Set the output directory to 'build'
     rollupOptions: {
       output: {
         manualChunks: {
@@ -14,6 +14,6 @@ export default defineConfig({
         }
       }
     },
-    chunkSizeWarningLimit: 1000 // Increase the limit to 1000 kB
+    // chunkSizeWarningLimit: 1000 // Increase the limit to 1000 kB
   }
 });
